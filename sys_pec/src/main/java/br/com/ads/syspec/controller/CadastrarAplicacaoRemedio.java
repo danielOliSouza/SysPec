6fdf692d5a926a7d7f96a87dad6a103ceb77cbb0
Daniel Oliveira Souza <daniel.backspace@gmail.com>
2017-07-17 13:28:52 -0300

    Initial - Managed Bean Cadastrar Aplicacao Remedio

--- sys_pec/src/main/java/br/com/ads/syspec/controller/CadastrarAplicacaoRemedio.java
+++ sys_pec/src/main/java/br/com/ads/syspec/controller/CadastrarAplicacaoRemedio.java
@@ -2,15 +2,48 @@ package br.com.ads.syspec.controller;
 
 import java.io.Serializable;
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 
 import javax.faces.view.ViewScoped;
 import javax.inject.Named;
 
 import br.com.ads.syspec.model.AplicacaoRemedio;
+import br.com.ads.syspec.model.Insumo;
 
 @Named
 @ViewScoped
 public class CadastrarAplicacaoRemedio implements Serializable{
+	private Date dtAplicacao;
+	private String tipoAplicacao;
+	private Insumo remedio;
+	private List<AplicacaoRemedio> aplicacaoes;
 
+	
+	//Getters And Setters
+	
+	public Date getDtAplicacao() {
+		return dtAplicacao;
+	}
+	public void setDtAplicacao(Date dtAplicacao) {
+		this.dtAplicacao = dtAplicacao;
+	}
+	public String getTipoAplicacao() {
+		return tipoAplicacao;
+	}
+	public void setTipoAplicacao(String tipoAplicacao) {
+		this.tipoAplicacao = tipoAplicacao;
+	}
+	public Insumo getRemedio() {
+		return remedio;
+	}
+	public void setRemedio(Insumo remedio) {
+		this.remedio = remedio;
+	}
+	public List<AplicacaoRemedio> getAplicacaoes() {
+		return aplicacaoes;
+	}
+	public void setAplicacaoes(List<AplicacaoRemedio> aplicacaoes) {
+		this.aplicacaoes = aplicacaoes;
+	}
 }
